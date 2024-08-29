aa83fa237db3e1ab62d73050edc8cb6f328bdce5
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2024-08-29 15:35:43 -0300

    Se agregan funciones y estructuras al contrato
    setSecureAddPercent
    __percentValue
    getSecureAddPercent
    __amount
    AddProduct
    isOwner

--- contracts/Meli.sol
+++ contracts/Meli.sol
@@ -28,4 +28,49 @@ contract Meli {
 
   Product[] private products;
   mapping(address => User) public users;
+
+  uint private _totalUsers;
+
+  event ProductPurchased(address indexed user, address owner, uint price);
+  event ProductAdded(address indexed owner, string name, uint price);
+
+  constructor(address _token) {
+    owner = msg.sender;
+    token = IERC20(_token);
+    _totalUsers = 0;
+  }
+
+  function setSecureAddPercent(uint percent) public isOwner {
+    secureAddPercent = percent;
+  }
+
+  function getSecureAddPercent() private view isOwner returns (uint) {
+    return secureAddPercent;
+  }
+
+  function __percentValue(uint _amount) public view returns (uint) {
+    return (secureAddPercent * _amount) / 100;
+  }
+
+  function __amount(uint _amount) private pure returns (uint) {
+    return _amount * (10 ** 18);
+  }
+
+  function AddProduct(
+    string memory name,
+    string memory desc,
+    string memory section,
+    uint price
+  ) {
+    // transferTokens(address(this), __amount(price));
+    products.push(
+      Product(name, desc, section, __amount(price), msg.sender, noOne)
+    );
+    emit ProductAdded(msg.sender, name, __amount(price));
+  }
+
+  modifier isOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 }
